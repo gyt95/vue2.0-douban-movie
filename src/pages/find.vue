f0c986bd7b204ecde16162c7665f25b7e212cda7
gyt95 <gyt95@163.com>
2017-09-26 23:59:07 +0800

    remove some useless pic && modify find.vue

--- src/pages/find.vue
+++ src/pages/find.vue
@@ -15,43 +15,8 @@
 			</router-link>
 		</div>
 		
-		<div class="recommend-box">
-			<div class="hot-recommend">
-				<a href="https://erebor.douban.com/redirect/?ad=186614&uid=&bid=y_-ELINqZ-s&unit=movie_home_left_bottom&crtr=3%3A%2F&mark=&hn=daisy9c&sig=6e67390d4698c17d087994f72fc4d915d65554c8805dc937c187ecfdf02b14f607ab6202b8104b9ed45b714390a21aa4dc84ec91c17b73e3c12ebcff023de091&pid=debug_fbbb9a6c7aa7f2cdd66900bdee9ecb92ac690ae5&target=https%3A%2F%2Fwww.douban.com%2Fbiz%2Fsony" target="_blank">
-					<img src="../assets/4740d58e67bfefd.jpg" alt="" width="100%">
-				</a>
-			</div>
-		</div>
-		<div class="interested-box">
-			
-		</div>
-		<div class="choose-box">
-			<p>选电影</p>
-			<div class="type">
-				<ul>
-					<li><router-link to="/home">经典</router-link></li>
-					<li><router-link to="/home">豆瓣高分</router-link></li>
-					<li><router-link to="/home">冷门佳片</router-link></li>
-				</ul>
-			</div>
-			<div class="type">
-				<ul>
-					<li><router-link to="/home">剧情</router-link></li>
-					<li><router-link to="/home">爱情</router-link></li>
-					<li><router-link to="/home">喜剧</router-link></li>
-					<li><router-link to="/home">科幻</router-link></li>
-					<li><router-link to="/home">动作</router-link></li>
-					<li><router-link to="/home">悬疑</router-link></li>
-					<li><router-link to="/home">治愈</router-link></li>
-				</ul>
-			</div>
-			<div class="type">
-				<ul style="border:0">
-					<li><router-link to="/home">日本</router-link></li>
-					<li><router-link to="/home">韩国</router-link></li>
-				</ul>
-			</div>
-		</div>
+		
+		
 		<!-- footer -->
 		<v-footer></v-footer>
 	</div>
@@ -107,30 +72,5 @@
 		    margin: 0.35rem;
 		}
 	}
-	.choose-box{
-		    margin-bottom: 2rem;
-		p{
-		    padding: 0 0.5rem;
-   			font-size: 0.8rem;
-		}
-		.type{
-			ul{
-			    border-bottom: 1px solid #f2f2f2;
-    			padding: 0.5rem 0.5rem 0;
-				li{
-					display:inline-block;
-				    margin-bottom: 0.5rem;
-					a{
-					    color: limegreen;
-					    font-size: 0.6rem;
-					    text-decoration: none;
-					    padding: 0.2rem 0.6rem;
-					    border-radius: 15px;
-					    border: 1px solid limegreen;
-					    display: inline-block;
-					}
-				}
-			}
-		}
-	}
+	
 </style>
